0eb40f77351d64292e04eb856270f715ee5492c0
Addy-95 <skabdullah.95@gmail.com>
2024-10-08 10:02:48 +0530

    sm8350-common: Address to some more packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,13 +5,21 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AICorePrebuilt-aicore_20240509.01_RC02 \
+    AiWallpapers \
+    AndroidAutoStubPrebuilt \
     Chrome \
     Chrome-Stub \
+    DevicePersonalizationPrebuiltPixel2024-U.21_P24_aiai_20240327.00_RC01 \
     Drive \
+    Flipendo \
     Maps \
+    NgaResources \
     PrebuiltGmail \
     RecorderPrebuilt \
     SafetyHubPrebuilt \
+    ScribePrebuilt \
+    WeatherPixelPrebuilt_24D1 \
     Videos \
     YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
